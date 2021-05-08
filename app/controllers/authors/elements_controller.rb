fe4ac0c35ad6bca0d8e2efaae585c7f4b7a1f967
mabsboza <marcosboza@moi-solutions.com>
2021-05-08 11:22:25 -0600

    Add element controller and some more changes

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -1,40 +1,25 @@
 module Authors
   class ElementsController < AuthorsController
-    before_action :set_element, only: [:show, :edit, :update, :destroy]
-
-    # GET /elements
-    def index
-      @elements = Element.all
-    end
-
-    # GET /elements/1
-    def show
-    end
-
-    # GET /elements/new
-    def new
-      @element = Element.new
-    end
-
-    # GET /elements/1/edit
-    def edit
-    end
+    before_action :set_post
+    before_action :set_element, only: [:update, :destroy]
 
     # POST /elements
     def create
-      @element = Element.new(element_params)
+      @element = @post.elements.build(element_params)
 
       if @element.save
-        redirect_to @element, notice: 'Element was successfully created.'
+        notice = nil
       else
-        render :new
+        notice = @element.errors.full_messages.join('.') << '.'
       end
+
+      redirect_to edit_post_path(@post), notice: notice
     end
 
-    # PATCH/PUT /elements/1
+  # PATCH/PUT /element/1
     def update
       if @element.update(element_params)
-        redirect_to @element, notice: 'Element was successfully updated.'
+        redirect_to edit_post_path(@post), notice: 'Element was successfully updated.'
       else
         render :edit
       end
@@ -47,9 +32,13 @@ module Authors
     end
 
     private
+
+      def set_post
+        @post = current_author.posts.find(params[:post_id])
+      end
       # Use callbacks to share common setup or constraints between actions.
       def set_element
-        @element = Element.find(params[:id])
+        @element = @post.elements.find(params[:id])
       end
 
       # Only allow a list of trusted parameters through.
