2940687a83a67b2ff06f685f62d15652ed628b21
mabsboza <marcosboza@moi-solutions.com>
2021-05-25 17:23:45 -0600

    Fix index page and some front changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@
 #
 class Post < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :title, use: [:slugged, :history, :finders]
 
   belongs_to :author
   has_many :elements
@@ -21,4 +21,16 @@ class Post < ApplicationRecord
   has_one_attached :header_image
   validates_presence_of :title, :description
   validates_length_of :description, within: 50..250
+
+  scope :published, -> do
+    where(published: true)
+  end
+
+  scope :more_recently_published, -> do
+    order(published_at: :desc)
+  end
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
